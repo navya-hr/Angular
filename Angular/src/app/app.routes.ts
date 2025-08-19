cb1e22dbaca2e01bf9455fcc085734ce6207a4d9
navya-hr <navyahr18@gmail.com>
2025-08-19 23:30:30 +0530

    Css applied code

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
+import { CssComponent } from './css-component/css-component';
+import { Css } from './css/css';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: App},
+    {path: "cssComp", component: CssComponent},
+    {path: "css", component: Css}
+];
---
+++ Angular/src/app/css/css.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-css',
+  imports: [],
+  templateUrl: './css.html',
+  styleUrl: './css.css'
+})
+export class Css {
+
+}
